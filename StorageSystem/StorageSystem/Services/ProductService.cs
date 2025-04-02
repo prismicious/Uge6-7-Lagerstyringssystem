5449fda3a1eebbbff20fed852453a4673a618d66
Kenneth Gorking <kgorking@outlook.com>
2025-04-02 11:06:00 +0200

    Created product service

--- StorageSystem/StorageSystem/Services/ProductService.cs
+++ StorageSystem/StorageSystem/Services/ProductService.cs
@@ -1,16 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using StorageSystem;
-using StorageSystem.Models;
+﻿using StorageSystem.Models;
 
 namespace StorageSystem.Services
 {
-    internal class ProductService
+    public static class ProductService
     {
-        static List<Product> GetProducts()
+        static List<Product> Get()
         {
             using (var ctx = new StorageContext())
             {
@@ -18,12 +12,39 @@ namespace StorageSystem.Services
             }
         }
 
-        static void AddProduct()
+        // Creates a new product. Returns the new product ID.
+        static int Create(decimal Price, string Name, string Type)
         {
             using (var ctx = new StorageContext())
             {
-                ctx.Products.Add(new Product { Price = 1.0m, Name = "test", Type = "meh" });
+                var p = new Product { Price = Price, Name = Name, Type = Type };
+                ctx.Products.Add(p);
                 ctx.SaveChanges();
+                return p.ID;
+            }
+        }
+
+        // Updates a product. Returns true if the database was updated.
+        static bool Update(Product p, decimal? Price = null, string? Name = null, string? Type = null)
+        {
+            using (var ctx = new StorageContext())
+            {
+                if (Price.HasValue)
+                    p.Price = Price.Value;
+                if (Name != null)
+                    p.Name = Name;
+                if (Type != null)
+                    p.Type = Type;
+                return 1 == ctx.SaveChanges();
+            }
+        }
+
+        // Removes a product from the database. Returns true if successfully removed from database.
+        static bool Remove(Product p)
+        {
+            using (var ctx = new StorageContext()) { 
+                ctx.Products.Remove(p);
+                return 1 == ctx.SaveChanges();
             }
         }
     }

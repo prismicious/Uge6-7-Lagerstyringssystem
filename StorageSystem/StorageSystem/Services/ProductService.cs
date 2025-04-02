291898837eb732e07f2e6981a0b2b45f6c7a7c4c
Kenneth Gorking <kgorking@outlook.com>
2025-04-02 10:33:51 +0200

    Added example

--- StorageSystem/StorageSystem/Services/ProductService.cs
+++ StorageSystem/StorageSystem/Services/ProductService.cs
@@ -3,10 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using StorageSystem;
+using StorageSystem.Models;
 
 namespace StorageSystem.Services
 {
     internal class ProductService
     {
+        static List<Product> GetProducts()
+        {
+            using (var ctx = new StorageContext())
+            {
+                return [.. ctx.Products];
+            }
+        }
+
+        static void AddProduct()
+        {
+            using (var ctx = new StorageContext())
+            {
+                ctx.Products.Add(new Product { Price = 1.0m, Name = "test", Type = "meh" });
+                ctx.SaveChanges();
+            }
+        }
     }
 }

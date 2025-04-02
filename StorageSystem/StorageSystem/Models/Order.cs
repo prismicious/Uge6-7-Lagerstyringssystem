715e97c1ddb21a3f969e34cdb1bb26a61fa2d361
Drejer1 <63230715+Drejer1@users.noreply.github.com>
2025-04-02 15:12:03 +0200

    Update branch

--- StorageSystem/StorageSystem/Models/Order.cs
+++ StorageSystem/StorageSystem/Models/Order.cs
@@ -6,7 +6,6 @@
         public int Quantity { get; set; }
         public decimal Discount { get; set; }
         public decimal Price { get; set; }
-
         public Product Product { get; set; }
         public OrderList OrderList { get; set; }
     }
--- StorageSystem/StorageSystem/Services/TransactionService.cs
+++ StorageSystem/StorageSystem/Services/TransactionService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using StorageSystem.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,30 @@ namespace StorageSystem.Services
 {
     internal class TransactionService
     {
+        static Transaction? GetTransactionByID(int id)
+        {
+            using (var ctx = new StorageContext())
+            {
+                return ctx.Transactions.SingleOrDefault(t => t.ID == id);
+            }
+            
+        }
+        static ICollection<Transaction>? GetWarehouseTransactions(int id)
+        {
+            using (var ctx = new StorageContext())
+            {
+                Warehouse? warehouse = ctx.Warehouses.Include(w => w.Transactions).FirstOrDefault(w => w.ID == id);
+
+                if (warehouse != null)
+                {
+                    ICollection<Transaction> result = warehouse.Transactions;
+                    return result;
+                } else
+                return null;
+            }
+        }
+
+        static 
+
     }
 }

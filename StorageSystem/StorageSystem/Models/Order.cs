b36e976cf5dc2fd91c66b586e7f53ef1cabaa9f8
Kenneth Gorking <kgorking@outlook.com>
2025-04-02 14:42:50 +0200

    Fixed some model relations bugs

--- StorageSystem/StorageSystem/Models/Order.cs
+++ StorageSystem/StorageSystem/Models/Order.cs
@@ -7,7 +7,9 @@
         public decimal Discount { get; set; }
         public decimal Price { get; set; }
 
+        public int ProductID { get; set; }
         public Product Product { get; set; }
+        public int OrderListID { get; set; }
         public OrderList OrderList { get; set; }
     }
 }

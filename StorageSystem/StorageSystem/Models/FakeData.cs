618bee867d337c7a23f43d062560df1e867a08d9
Mikkel Sahlholdt <59990230+prismicious@users.noreply.github.com>
2025-04-03 12:01:38 +0200

    update fakedata to use namespace

--- StorageSystem/StorageSystem/Models/FakeData.cs
+++ StorageSystem/StorageSystem/Models/FakeData.cs
@@ -2,8 +2,11 @@ using Bogus;
 using StorageSystem;
 using StorageSystem.Models;
 
-public static class FakeData
+namespace StorageSystem.Models
 {
-    public static List<Product> Products { get; set; } = new List<Product>();
-    public static List<Customer> Customers { get; set; } = new List<Customer>();
+    public static class FakeData
+    {
+        public static List<Product> Products { get; set; } = new List<Product>();
+        public static List<Customer> Customers { get; set; } = new List<Customer>();
+    }
 }

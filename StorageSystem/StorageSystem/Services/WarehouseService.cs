31d0a6bc033e61ce959a4f36a811476289ccbcb5
Kenneth Gorking <kgorking@outlook.com>
2025-04-04 13:44:05 +0200

    Needs transaction service

--- StorageSystem/StorageSystem/Services/WarehouseService.cs
+++ StorageSystem/StorageSystem/Services/WarehouseService.cs
@@ -1,4 +1,5 @@
-﻿using StorageSystem;
+﻿using Microsoft.EntityFrameworkCore;
+using StorageSystem;
 using StorageSystem.Models;
 
 namespace StorageSystem.Services
@@ -8,11 +9,130 @@ namespace StorageSystem.Services
         public static Warehouse Create(string location)
         {
             using var ctx = new StorageContext();
+
+            // Create the warehouse
             var warehouse = new Warehouse { Location = location };
             ctx.Warehouses.Add(warehouse);
             ctx.SaveChanges();
+
+            // Create customer for the new warehouse
+            var wh_customer = new Customer { Address = location, Name = $"warehouse:{warehouse.ID}", Email = "internal@warehouse.storage", Type = 2 };
+            ctx.Customers.Add(wh_customer);
+            ctx.SaveChanges();
+
             return warehouse;
         }
 
+        public static bool Remove(Warehouse wh)
+        {
+            using var ctx = new StorageContext();
+            ctx.Warehouses.Remove(wh);
+            return 1 == ctx.SaveChanges();
+        }
+
+        // Get the customer associated with the warehouse
+        public static Customer GetAssociatedCustomer(Warehouse wh)
+        {
+            using var ctx = new StorageContext();
+            return ctx.Customers
+                .Where(c => c.Name == $"warehouse:{wh.ID}")
+                .Single();
+        }
+
+        // Get the product status for a product
+        public static ProductStatus? GetProductStatus(Warehouse wh, Product product)
+        {
+            return GetProductStatus(wh, product.ID);
+        }
+
+        // Get the product status for a product
+        public static ProductStatus? GetProductStatus(Warehouse wh, int productID)
+        {
+            using var ctx = new StorageContext();
+            return ctx.ProductStatuses
+                .Include(ps => ps.Warehouse)
+                .Where(ps => ps.ProductID == productID && ps.Warehouse == wh)
+                .SingleOrDefault();
+        }
+
+        // Create new product status for a product
+        public static ProductStatus? CreateProductStatus(Warehouse wh, Product p, int initialQuantity = 0)
+        {
+            using var ctx = new StorageContext();
+            var ps = new ProductStatus { ProductID = p.ID, WarehouseID = wh.ID, Quantity = initialQuantity };
+            ctx.ProductStatuses.Add(ps);
+            if (1 != ctx.SaveChanges())
+                return null;
+            else
+                return ps;
+        }
+
+        // Updates a product status for a product
+        public static bool UpdateProductStatus(ProductStatus ps)
+        {
+            using var ctx = new StorageContext();
+
+            if (ps.ID == 0 || !ctx.ProductStatuses.Contains(ps))
+                return false;
+
+            ctx.ProductStatuses.Update(ps);
+            return 1 == ctx.SaveChanges();
+        }
+
+        // Update a products quantity in a warehouse.
+        // Returns true if the quantity changed the warehouse stock.
+        public static bool UpdateProductQuantity(Warehouse wh, Product p, int quantity)
+        {
+            using var ctx = new StorageContext();
+
+            // Find the ProductStatus object associated with the warehouse and product
+            ProductStatus? productStatus = ctx.ProductStatuses
+                .Include(ps => ps.Warehouse)
+                .Where(ps => ps.ProductID == p.ID && ps.Warehouse.ID == wh.ID)
+                .SingleOrDefault();
+
+            if (null == productStatus)
+            {
+                // Make sure we don't create a product status with negative quantity
+                if (quantity < 0)
+                    return false;
+
+                productStatus = CreateProductStatus(wh, p, quantity);
+                return productStatus != null;
+            }
+            else
+            {
+                // Ensure there is enough stock to
+                if (quantity < 0 && quantity < -productStatus.Quantity)
+                    return false;
+
+                productStatus.Quantity += quantity;
+                return UpdateProductStatus(productStatus);
+            }
+        }
+
+        // Moves product from one warehouse to another
+        public static bool MoveProduct(Warehouse from, Warehouse to, Product p, int quantity)
+        {
+            ProductStatus? statusFrom = GetProductStatus(from, p);
+
+            // If there is no product status, there is no product at location
+            if (statusFrom == null)
+                return false;
+
+            // Make sure there is enough product to move
+            if (statusFrom.Quantity < quantity)
+                return false;
+
+            // Create order for the move
+            var customerTo = GetAssociatedCustomer(to);
+            var ol = OrderListService.Create(customerTo);
+            var order = OrderService.Create(ol, p, quantity, 0, 0); // special price for friends
+
+            // TODO create the transaction
+            //var transaction = TransactionService.Create(ol);
+
+            return true;
+        }
     }
 }
---
+++ StorageSystem/StorageSystemTest/TestWarehouseService.cs
@@ -0,0 +1,100 @@
+﻿using StorageSystem;
+using StorageSystem.Models;
+using StorageSystem.Services;
+
+namespace StorageSystemTest
+{
+    [TestClass]
+    public class TestWarehouseService
+    {
+        Warehouse wh1, wh2;
+        List<Product> products;
+        List<Customer> customers;
+
+        [TestInitialize]
+        public void Init()
+        {
+            using var ctx = new StorageContext();
+            ctx.Database.EnsureCreated();
+
+            // Create some test warehouses
+            wh1 = WarehouseService.Create("test:location 1");
+            wh2 = WarehouseService.Create("test:location 2");
+            Assert.AreEqual(2, ctx.Warehouses.Count());
+
+            // Create fake products and customers if needed
+            if (ctx.Customers.Count() == 0 || ctx.Products.Count() == 0)
+                FakerService.Generate();
+
+            products = ctx.Products.ToList();
+            customers = ctx.Customers.ToList();
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            // Remove warehouses. Should also remove all product statuses
+            WarehouseService.Remove(wh1);
+            WarehouseService.Remove(wh2);
+            
+            using var ctx = new StorageContext();
+            Assert.AreEqual(0, ctx.Warehouses.Count());
+        }
+
+        // Make sure that creating a warehouse also creates an internal customer
+        [TestMethod]
+        public void WarehouseCustomerExists()
+        {
+            using var ctx = new StorageContext();
+            string name1 = $"warehouse:{wh1.ID}";
+            string name2 = $"warehouse:{wh2.ID}";
+
+            int numWarehouseCustomers = ctx.Customers.Where(c => c.Name == name1 || c.Name == name2).Count();
+            Assert.AreEqual(2, numWarehouseCustomers);
+        }
+
+        [TestMethod]
+        public void CreateProductStatuses()
+        {
+            // Create product statuses in each warehouse.
+            // The quantity for each product is its ID.
+            var products = ProductService.Get();
+            foreach (var p in products)
+            {
+                WarehouseService.CreateProductStatus(wh1, p, p.ID);
+                WarehouseService.CreateProductStatus(wh2, p, p.ID);
+            }
+
+            // Ensure the number of product statuses created is correct
+            using var ctx = new StorageContext();
+            Assert.AreEqual(2 * products.Count, ctx.ProductStatuses.Count());
+
+            // Ensure the quantity for each product is correct
+            foreach (var p in products)
+            {
+                int? quant1 = WarehouseService.GetProductStatus(wh1, p.ID)?.Quantity;
+                int? quant2 = WarehouseService.GetProductStatus(wh2, p.ID)?.Quantity;
+                Assert.AreEqual(p.ID, quant1);
+                Assert.AreEqual(p.ID, quant2);
+            }
+
+            // Double the quantities and re-check
+            foreach (var p in products)
+            {
+                WarehouseService.UpdateProductQuantity(wh1, p, p.ID);
+                WarehouseService.UpdateProductQuantity(wh2, p, p.ID);
+
+                int? quant1 = WarehouseService.GetProductStatus(wh1, p.ID)?.Quantity;
+                int? quant2 = WarehouseService.GetProductStatus(wh2, p.ID)?.Quantity;
+                Assert.AreEqual(2 * p.ID, quant1);
+                Assert.AreEqual(2 * p.ID, quant2);
+            }
+        }
+
+        [TestMethod]
+        public void MoveBetweenWarehouses()
+        {
+            // TODO needs transaction service
+        }
+    }
+}

9bd882931a30efba70fa94702b0132402c708ed4
Kenneth Gorking <kgorking@outlook.com>
2025-04-04 10:04:27 +0200

    First commit

---
+++ StorageSystem/StorageSystem/Services/WarehouseService.cs
@@ -0,0 +1,18 @@
+﻿using StorageSystem;
+using StorageSystem.Models;
+
+namespace StorageSystem.Services
+{
+    public static class WarehouseService
+    {
+        public static Warehouse Create(string location)
+        {
+            using var ctx = new StorageContext();
+            var warehouse = new Warehouse { Location = location };
+            ctx.Warehouses.Add(warehouse);
+            ctx.SaveChanges();
+            return warehouse;
+        }
+
+    }
+}

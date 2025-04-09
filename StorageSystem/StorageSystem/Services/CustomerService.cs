327b98400aa1fd1bef13a453a58c0c9b07e2511b
Kenneth Gorking <kgorking@outlook.com>
2025-04-09 14:59:23 +0200

    Start of customer service

---
+++ StorageSystem/StorageSystem/Services/CustomerService.cs
@@ -0,0 +1,13 @@
+﻿using StorageSystem.Models;
+
+namespace StorageSystem.Services
+{
+    public static class CustomerService
+    {
+        public static Customer Create();
+        public static Customer Get(int ID, bool includeOrderList = false, bool includeReceipts = false);
+        public static List<Customer> Get(bool includeOrderList = false, bool includeReceipts = false);
+        public static bool Update(Customer c);
+        public static bool Delete(Customer c);
+    }
+}

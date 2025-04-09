a710091fb8f38381a0473da77a3fe8fb27ce0b4a
Drejer1 <63230715+Drejer1@users.noreply.github.com>
2025-04-09 14:59:21 +0200

    Create CustomerService.cs

---
+++ StorageSystem/StorageSystem/Services/CustomerService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using StorageSystem.Models;
+
+namespace StorageSystem.Services
+{
+    class CustomerService 
+    {
+        public static Customer? getCustomer(int id)
+        {
+            using (var ctx = new StorageContext())
+            {
+                return ctx.Customers.Where(p => p.ID == id).SingleOrDefault();
+            }
+        }
+        public static List<Receipt>? getCustomerReceipts(int id)
+        {
+            using (var ctx = new StorageContext())
+            {
+                return ctx.Customers
+                    .Where(c => c.ID ==  id)
+                    .SelectMany(c => c.OrderLists)
+                    .Select(t => t.Transaction)
+                    .Where()
+            }
+        }
+
+        public static void deleteCustomer()
+        {
+
+        }
+
+        public static Customer? createCustomer()
+        {
+
+        }
+
+    }
+}

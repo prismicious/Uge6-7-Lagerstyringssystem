a6b5cb20e43f733d40c236fe54dae2a9d68c9cf8
Kenneth Gorking <kgorking@outlook.com>
2025-04-09 14:33:58 +0200

    Less stress, more comments

--- StorageSystem/StorageSystemTest/TestWarehouseService.cs
+++ StorageSystem/StorageSystemTest/TestWarehouseService.cs
@@ -96,27 +96,32 @@ namespace StorageSystemTest
         [TestMethod]
         public void MoveBetweenWarehouses()
         {
-            foreach (var p in products)
-            {
-                WarehouseService.UpdateProductQuantity(wh1, p, p.ID);
-                WarehouseService.UpdateProductQuantity(wh2, p, p.ID);
-            }
-
-            // Move product to a single warehouse
+            // The product to move
+            Assert.IsTrue(products.Count > 5);
             var product = products[5];
+
+            // Make sure there is product in each warehouse
+            WarehouseService.UpdateProductQuantity(wh1, product, product.ID);
+            WarehouseService.UpdateProductQuantity(wh2, product, product.ID);
+
+            // Get the product status for the product, from both warehouses
             var status_wh1 = WarehouseService.GetProductStatus(wh1, product);
             var status_wh2 = WarehouseService.GetProductStatus(wh2, product);
             Assert.IsNotNull(status_wh1);
             Assert.IsNotNull(status_wh2);
 
+            // Get the quantities
             int quantity_wh1 = status_wh1.Quantity;
             int quantity_wh2 = status_wh2.Quantity;
             Assert.AreNotEqual(0, quantity_wh1);
             Assert.AreNotEqual(0, quantity_wh2);
 
-            // Move products from wh1 to wh2
-            var orderList = OrderListService.Create(WarehouseService.GetAssociatedCustomer(wh2));
+            // Create an order list for the product
+            var wh2Customer = WarehouseService.GetAssociatedCustomer(wh2);
+            var orderList = OrderListService.Create(wh2Customer);
             var order = OrderService.Create(orderList, product, quantity_wh1, 0, 0);
+
+            // Use the order list to move the product from wh1 to wh2
             var tx = WarehouseService.MoveProduct(wh1, wh2, orderList);
 
             // Check that product was moved
@@ -125,9 +130,14 @@ namespace StorageSystemTest
             Assert.IsNotNull(status_wh1);
             Assert.IsNotNull(status_wh2);
 
+            // Warehouse 1 should have zero quantity
             Assert.AreEqual(0, status_wh1.Quantity);
-            Assert.AreEqual(quantity_wh1 + quantity_wh2, status_wh2.Quantity);
+
+            // Warehouse 2 should have total quantity
+            int quantTotal = quantity_wh1 + quantity_wh2;
+            Assert.AreEqual(quantTotal, status_wh2.Quantity);
         }
+
         [TestMethod]
         public void GetAllProductStatuses()
         {
@@ -139,9 +149,9 @@ namespace StorageSystemTest
                 index++;
             }
             
-            List<ProductStatus> result = WarehouseService.GetAllWarehouseProductStatus(wh1);
+            List<ProductStatus>? result = WarehouseService.GetAllWarehouseProductStatus(wh1);
+            Assert.IsNotNull(result);
             Assert.AreEqual(100, result.Count);
         }
-
     }
 }

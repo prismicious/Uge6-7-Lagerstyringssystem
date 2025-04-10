b60b67652d9ffe6fe1483b27a705b5645c98134f
Kenneth Gorking <kgorking@outlook.com>
2025-04-10 09:30:10 +0200

    Transaction service (starten på det) (#58)
    * Update branch
    
    * Deactivate parallel tests
    
    * Created 2 gets and accompanying tests
    
    * Fix update til at bruge patch.
    
    * Cleaned up delete
    
    * Removed unneed code
    
    * Testing
    
    * Initial support for transaction service
    
    ---------
    
    Co-authored-by: Drejer1 <63230715+Drejer1@users.noreply.github.com>

--- StorageSystem/StorageSystem/Services/TransactionService.cs
+++ StorageSystem/StorageSystem/Services/TransactionService.cs
@@ -1,12 +1,59 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using StorageSystem.Models;
 
 namespace StorageSystem.Services
 {
-    internal class TransactionService
+    public class TransactionService
     {
+        static public Transaction? Get(int id)
+        {
+            using (var ctx = new StorageContext())
+            {
+                return ctx.Transactions.SingleOrDefault(t => t.ID == id);
+            }
+        }
+
+        static public ICollection<Transaction>? GetWarehouseTransactions(int warehouseId)
+        {
+            using (var ctx = new StorageContext())
+            {
+                Warehouse? warehouse = ctx.Warehouses
+                    .Include(w => w.Transactions)
+                    .SingleOrDefault(w => w.ID == warehouseId);
+
+                return warehouse?.Transactions;
+            }
+        }
+
+        static public Transaction Create(OrderList orderList, Helpers.TransactionType type)
+        {
+            using var ctx = new StorageContext();
+
+            // Check to see if the order list already have a transaction.
+            // If it does, return it.
+            Transaction? tx = ctx.Transactions.Where(t => t.OrderList == orderList).SingleOrDefault();
+            if (tx != null)
+                return tx;
+
+            tx = new Transaction { OrderList = orderList, Type = type, Date = DateTime.Now };
+            ctx.Transactions.Add(tx);
+            ctx.SaveChanges();
+            return tx;
+        }
+    
+        static public Transaction CreateSale(OrderList orderList)
+        {
+            return Create(orderList, Helpers.TransactionType.Sale);
+        }
+
+        static public Transaction CreateReturn(OrderList orderList)
+        {
+            return Create(orderList, Helpers.TransactionType.Return);
+        }
+
+        static public Transaction CreateWarehouseTransfer(OrderList orderList)
+        {
+            return Create(orderList, Helpers.TransactionType.Transfer);
+        }
     }
 }

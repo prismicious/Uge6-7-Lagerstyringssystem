827d551b8e025c96850c1c047a919f917107a297
Kenneth Gorking <kgorking@outlook.com>
2025-04-10 11:09:20 +0200

    xx

--- StorageSystem/StorageSystemTest/TestEndToEnd.cs
+++ StorageSystem/StorageSystemTest/TestEndToEnd.cs
@@ -1,4 +1,6 @@
 using StorageSystem;
+using StorageSystem.Helpers;
+using StorageSystem.Models;
 using StorageSystem.Services;
 namespace StorageSystemTest;
 
@@ -21,17 +23,26 @@ public class TestEndToEnd
         var product = ProductService.Create(2.34m, "Example product", "-");
 
         // Create 2 warehouses
-        //var warehouse_1 = WarehouseService.
+        var warehouse_1 = WarehouseService.Create("Aarhus");
+        var warehouse_2 = WarehouseService.Create("Ballerup");
 
-        // Create a product status for the product in both warehouses
+        // Create a product status for the product in both warehouses.
+        // Add 5 to each warehouse.
+        WarehouseService.CreateProductStatus(warehouse_1, product, 5);
+        WarehouseService.CreateProductStatus(warehouse_2, product, 5);
+
 
         // Create a customer
+        var customer = CustomerService.Create("Knud Karate", "cirkelspark@ansigt.dk", "Kindhestegade 23", CustomerType.Normal);
+        Assert.IsNotNull(customer);
 
-        // Select an appropiate warehouse for the customer
+        // ** Select an appropiate warehouse for the customer
 
         // Create an orderlist for the customer
+        var orderlist = OrderListService.Create(customer);
 
         // Create an order and add them to the orderlist
+        var order = OrderService.Create(orderlist, product, 10, 0, product.Price);
 
         // Create a transaction when the customer pays
 

4d22595caba133d52f605bdc099d0342dec53d6f
Kenneth Gorking <kgorking@outlook.com>
2025-04-10 10:48:49 +0200

    Set up actions

--- StorageSystem/StorageSystemTest/TestEndToEnd.cs
+++ StorageSystem/StorageSystemTest/TestEndToEnd.cs
@@ -1,10 +1,43 @@
+using StorageSystem;
+using StorageSystem.Services;
 namespace StorageSystemTest;
 
 [TestClass]
 public class TestEndToEnd
 {
-    [TestMethod]
-    public void TestMethod1()
+    [TestInitialize]
+    public void Init()
     {
+        // Reset the database
+        using var ctx = new StorageContext();
+        ctx.Database.EnsureDeleted();
+        ctx.Database.EnsureCreated();
+    }
+
+    [TestMethod]
+    public void EndToEnd()
+    {
+        // Create 1 example product
+
+        // Create 2 warehouses
+
+        // Create a product status for the product in both warehouses
+
+        // Create a customer
+
+        // Select an appropiate warehouse for the customer
+
+        // Create an orderlist for the customer
+
+        // Create an order and add them to the orderlist
+
+        // Create a transaction when the customer pays
+
+        // Neither warehouse has enough stock to fulfill the order,
+        // so move product from one warehouse to the other
+        // - OR -
+        // Create two transactions, one for each warehouse
+
+        // Create a receipt for the transaction when the warehouse ships out the product
     }
 }
